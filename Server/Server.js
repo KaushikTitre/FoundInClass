106a8e1481b923f595a412f9918c684b9325cda6
Kaushik Titre <kaushiktitre@gmail.com>
2025-10-12 18:55:42 +0530

    Fix Dashboard import path casing for Vercel

--- Server/Server.js
+++ Server/Server.js
@@ -8,7 +8,7 @@ import postRoutes from "./routes/postRoutes.js"
 import userRoutes from "./routes/userRoutes.js"
 
 const app = express();
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://found-in-class.vercel.app/", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
